45815c1eae8dcd14f1eba778107a2d78a537f008
Phagun Jain <omgill084@gmail.com>
2021-02-08 17:29:34 +0530

    messing up with eclipse

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,9 +1,6 @@
-package fOReXPORT;
 
-public class test {
-
+public class hello {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 	}
 }
